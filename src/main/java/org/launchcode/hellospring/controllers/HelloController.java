0de1a5db024668cf4880735b6081c64298b83331
maks075 <shlapak.maksym@gmail.com>
2022-11-08 17:32:24 -0600

    using path param

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,6 +30,14 @@ public class HelloController {
 
         return "Hello, " + name + "!";
 
+    }
+
+    //handles requests of the form /hello/LaunchCode
+
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 
 }
