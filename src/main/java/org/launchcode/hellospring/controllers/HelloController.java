c78676659497f1ded6d6230ed9f7e531315b9b01
maks075 <shlapak.maksym@gmail.com>
2022-11-10 19:33:53 -0600

    controllers and routing exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -22,9 +22,10 @@ public class HelloController {
     //Handles request of the form /hello?name=LaunchCode
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    public String helloWithQueryParam(@RequestParam String name) {
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+        String properGreeting = HelloController.createMessage(name, language);
 
-        return "Hello, " + name + "!";
+        return "<h3 style='color:blue'>" + properGreeting + "</h3>";
 
     }
 
@@ -41,10 +42,46 @@ public class HelloController {
                 "<body>" +
                 "<form action='hello' method='post'>" +  //submit a request to /hello
                 "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                    "<option value='English'>English</option>" +
+                    "<option value='Ukrainian'>Ukrainian</option>" +
+                    "<option value='German'>German</option>" +
+                    "<option value='French'>French</option>" +
+                    "<option value='Spanish'>Spanish</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+
+    //@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public static String createMessage(String n, String l) {
+        String message = "";
+
+        if (l.equals("English") ) {
+            message = "Hello " + n;
+        }
+
+        if (l.equals("Ukrainian") ) {
+            message = "Привіт " + n;
+        }
+
+        if (l.equals("German") ) {
+            message = "Hallo " + n;
+        }
+
+        if (l.equals("French") ) {
+            message = "Bonjour " + n;
+        }
+
+        if (l.equals("Spanish")  ) {
+            message = "Hola " + n;
+        }
+
+        return message;
+    }
+
+
 
 }
